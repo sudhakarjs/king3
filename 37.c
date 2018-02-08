0a125e711e22dc041c30e7dc5ed2afd7c48c7527
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-08 09:35:50 +0000

    Create 37.c

---
+++ 37.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+#include<conio.h>
+ void main()
+ {
+ char a[20]
+ int flag=0,b,i,a1:
+ clrscr();
+ printf("\n Enter the string language");
+ scanf("%d,&b);
+ printf(Enter the string");
+ scanf("%s",b);
+ for(1=0;i<b;i++)
+ {
+ if ((int a[i]>47&&(int)a[i]<58)
+ flag++
+ }
+ if(flag==0)
+ printf("\n the string has numeric");
+ if(flag>0)
+ printf("The string has %d number",flag)
+ getch()
+ }
+
