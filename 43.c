758b54604fbb7a99a36dc8593ca21eac0948574b
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-09 13:52:53 +0530

    Create 43.c

---
+++ 43.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    char a[100]="sampath";
+    printf("how many time ")
+    scanf("%d",&a);
+    for(b=1;b<=a;b++)
+    {
+        printf("%s\n",a);
+    }
+}
