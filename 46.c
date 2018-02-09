10e16438db94289b78ca512fc520b916dcb002af
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-09 13:59:10 +0530

    Create 46.c

---
+++ 46.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter the value of a");
+scanf("%d",&a);
+for(a=1;a<=10;a++)
+{
+printf("%d",a);
+}
+if(b==a)
+{
+pritf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
+
