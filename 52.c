f627e5745b8cf7d332fb9027e87be0d46efee764
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-09 14:12:54 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int b,i=1,c;
+    printf("Enter the number");
+    scanf("%d",&a);
+    for(c=1;c<a;c++)
+    {
+        i=i*2;
+        if(i==a)
+        printf("Yes");
+    
+    }
+}
