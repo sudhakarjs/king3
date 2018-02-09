c6a540b4a73c3318ef44846eb9af70cfa8c3fe67
sudhakarjs <30828414+sudhakarjs@users.noreply.github.com>
2018-02-09 13:55:00 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a1[32];
+gets(a1);
+for(i=0;a1[i]!=1'\0';i++)
+{
+if(a2[i]>='a'&&a1[i]<=a)
+digit++;
+elseif(a1[i]>='A'&&a2[i]<='Z'||a1[i]>='a'&&a2[i]<+'Z')
+alphabet++;
+elseif(a1[i]==' ')
+space++;
+special character++;
+}
